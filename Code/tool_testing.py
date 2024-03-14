a8d8282641ec499ba707f58bfa0a33605beb3005
drakomanios <drakomanios@gmail.com>
2024-03-14 22:55:07 +0200

    changed to domain instead of duration

--- Code/tool_testing.py
+++ Code/tool_testing.py
@@ -1,2 +1,16 @@
-import tools as t
+import tools as tl
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
+
+w1 = tl.Sinewave(3,2,0)
+w2 = tl.Sinewave(3,2,0,-4,6)
+w3 = tl.Sinewave(3,7,0,-1,1)
+w4 = w1.superposition([w2,w3])
+w5 = w1.convolve([w2,w3])
+
+# print(w1.t,w2.y)
+# print(w4.t,w4.y)
+plt.plot(w4.t,w4.y)
+plt.show()
+
--- Code/tools.py
+++ Code/tools.py
@@ -4,61 +4,53 @@ from scipy.fft import fft, fftfreq
 import copy
 
 class Wave:
-    def __init__(self,duration=6,step=1/100_000):
-        self.d = duration     ##Change to Span 
+    def __init__(self,start=-np.pi,end=np.pi,step=100_000):
+        self.start = start
+        self.end = end  ##Changed to domain
+        self.dlength = self.end-self.start #Lenght of the domain
         self.step = step
-        self.t = np.linspace(0,self.d,int(self.d/self.step),endpoint = False)
+        self.t = np.linspace(self.start,self.end,int(self.dlength*self.step))
+
+    def setDomain(self,waves:list):
+        waves.append(self)
+        start = waves[0].start
+        end = waves[0].end
+        for wave in waves:
+            if wave.start < start:
+                start = wave.start
+            if wave.end > end:
+                end = wave.end
+        return Wave(start,end)
     
-    def convolve(self,other_waves):
-        maxlen = 0
-        maxwave = 0
-        other_waves.append(self)
-        for i in range(len(other_waves)):
-            if len(other_waves[i].y) > maxlen:
-                maxlen = len(other_waves[i].y)
-                maxwave = other_waves[i]
-                
-        convolved_wave = Wave(duration = maxwave.d)
-        yc =maxwave.y
-        other_waves.remove(maxwave)
-        for wave in other_waves:
-            yc = signal.fftconvolve(yc,wave.y,'same')*(self.step*self.d)**0.5
+    def convolve(self,waves:list):     
+        convolved_wave = self.setDomain(waves)
+        yc = signal.unit_impulse(len(convolved_wave.t),'mid')
+        for wave in waves:
+            yc = signal.fftconvolve(yc,wave.y,'same')/(self.step*convolved_wave.dlength)**0.5
         convolved_wave.y = yc
         return convolved_wave
         
-    def superposition(self, other_waves):
-        maxlen = 0
-        maxwave = 0
-        other_waves.append(self)
-        temp_waves = copy.deepcopy(other_waves)
-        for i in range(len(temp_waves)):
-            if len(temp_waves[i].y) > maxlen:
-                maxlen = len(temp_waves[i].y)
-                maxwave = temp_waves[i]
-        
-        superposed_wave = Wave(duration = maxwave.d)
-        ys = 0
-        for wave in temp_waves:
-            if len(wave.y) < maxlen:
-                print(len(wave.y))
-                wave.y = np.append(wave.y,np.zeros(maxlen-len(wave.y)))
-                print(len(wave.y))
-                ys += wave.y
-            elif len(wave.y) == maxlen:
-                ys += wave.y
+    def superposition(self, waves:list):
+        #temp_waves = copy.deepcopy(waves)
+        superposed_wave = self.setDomain(waves)
+        ys=np.zeros(int(np.ceil(superposed_wave.dlength*self.step))) #array filled with max domain lenght zeros, rounded up
+        for wave in waves:
+            start_diff = wave.start - superposed_wave.start #calculate start difference
+            add_from = int(np.ceil(start_diff*self.step)) #find start index, rounded up
+            ys[add_from:add_from+len(wave.y)] += wave.y
         superposed_wave.y = ys
         return superposed_wave
     
     def fft(self,freq): ##ADD  parameter freq
         N = len(self.y)
-        self.yf = np.fft.rfft(self.y)[:N//2]*2*self.step/self.d
+        self.yf = np.fft.rfft(self.y)[:N//2]*2*self.step/self.dlength
         self.tf = np.fft.rfftfreq(N, np.diff(self.t)[0])[:N//2]
         return (self.tf, abs(self.yf)) 
     
         
 class Sinewave(Wave):
-    def __init__(self,frequency,amplitude=1,phase=0,duration=6,step=1/100_000):
-        super().__init__(duration,step)
+    def __init__(self,frequency,amplitude=1,phase=0,start=-np.pi,end=np.pi,step=100_000):
+        super().__init__(start,end,step)
         self.a = amplitude
         self.phase = phase
         self.f = frequency
@@ -71,22 +63,21 @@ class Sinewave(Wave):
 
 
 class Square_wave(Wave): 
-    def __init__(self,frequency,amplitude=1,phase=0,duration=6,step=1/100_000):
-      super().__init__(duration,step)
+    def __init__(self,frequency,amplitude=1,phase=0,start=-np.pi,end=np.pi,step=100_000):
+      super().__init__(start,end,step)
       self.a = amplitude
       self.phase = phase
       self.f = frequency
       self.y = self.a * signal.square(np.sin(2*np.pi*self.f*self.t - self.phase))
       self.t_for_fft = np.linspace(0, 1/(self.step*4*self.f), int(1/self.step) , endpoint=False)
       self.y_for_fft = self.a * signal.square(np.sin(2*np.pi*self.f*self.t_for_fft - self.phase))
-      
         
       def __str__(self):
         return f'square wave with:\nfrequency = {self.f}\namplitude = {self.a}\nphase = {self.phase}'
 
 class Trianglewave(Wave):
-   def __init__(self,frequency,amplitude=1,phase=0,duration=6,step=1/100_000):
-      super().__init__(duration,step)
+   def __init__(self,frequency,amplitude=1,phase=0,start=-np.pi,end=np.pi,step=100_000):
+      super().__init__(start,end,step)
       self.a = amplitude
       self.phase = phase
       self.f = frequency
